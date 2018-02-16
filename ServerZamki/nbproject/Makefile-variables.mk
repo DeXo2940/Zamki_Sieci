28facb8320b1644f22dac7445875d45eb3cbfa90
sovly <sovly@Sovly-PC>
2018-02-16 14:52:59 +0100

    Dodanie projektu na server
    Bartek

---
+++ ServerZamki/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
+CND_ARTIFACT_NAME_Debug=serverzamki
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/serverzamki
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
+CND_PACKAGE_NAME_Debug=serverzamki.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/serverzamki.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
+CND_ARTIFACT_NAME_Release=serverzamki
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/serverzamki
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
+CND_PACKAGE_NAME_Release=serverzamki.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/serverzamki.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
