1a5ef9a914067e0b20057fc67a4b535982bb4367
sovly <sovly@Sovly-PC>
2018-02-16 16:56:58 +0100

    Dodanie kodu w team.hpp/.cpp Bartek

--- ServerZamki/cpp_classes/team.cpp
+++ ServerZamki/cpp_classes/team.cpp
@@ -1,7 +1,31 @@
 #include "team.hpp"
 
-Team::Team() {
-
+Team::Team(int id, char color) {
+    this->id = id;
+    this->color = color;
+    this->castle=new Castle();
 }
+
+int Team::getId() {
+    return this->id;
+}
+
+char Team::getColor() {
+    return this->color;
+}
+
+void Team::addToTeam(int nfds) {
+    membersNfds.push_back(nfds);
+}
+
+void Team::removeFromTeam(int nfds) {
+    for (int i = 0; i < membersNfds.size(); ++i) {
+        if (membersNfds.at(i) == nfds) {
+            membersNfds.erase(membersNfds.begin() + i);
+            break;
+        }
+    }
+}
+
 
 
--- ServerZamki/cpp_classes/team.hpp
+++ ServerZamki/cpp_classes/team.hpp
@@ -1,13 +1,23 @@
-
 #ifndef TEAM_H
 #define TEAM_H
+#include <vector>
+
+#include "castle.hpp"
+using namespace std;
 
 class Team {
+    int id;
+    char color;
+    vector<int> membersNfds;
+    Castle* castle;
 public:
-    Team();
-
-private:
-    /* add your private declarations */
+    Team(int id,char color);
+    char getColor();
+    int getId();
+    
+    void addToTeam(int nfds);
+    void removeFromTeam(int nfds);
+    
 };
 
 #endif /* TEAM_H */ 
