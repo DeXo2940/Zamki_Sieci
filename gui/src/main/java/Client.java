42e902f9f8653f7ea8700b0d0111aa03d1bf58d2
huehehehe <sowailime@gmail.com>
2018-02-25 23:33:14 +0100

    Kilka zakodowanych komunikatów i testów jednostkowych

--- gui/src/main/java/Client.java
+++ gui/src/main/java/Client.java
@@ -8,14 +8,12 @@ public class Client implements Runnable {
 
     private Socket socket;
     private Integer teamNumber = 0;
-    private String nick;
 
     public Client() {
 
     }
 
     public void doIt(String server, int serverPort) throws Exception {
-
         this.socket = new Socket(InetAddress.getByName(server), serverPort);
         this.run();
     }
@@ -23,6 +21,57 @@ public class Client implements Runnable {
     public boolean isConnected() {
         if(socket.isConnected()) return true;
         else return false;
+    }
+
+
+    public boolean checkCommunicate(String com) {
+        if (com.length() != 5) return false;
+        if (com.charAt(0) == com.charAt(com.length()-1)) return true;
+        return false;
+    }
+
+    public void getMessage () {
+
+    }
+
+    public char ifImNew(String communicate) {
+        if (communicate.charAt(0) == 't') {
+            this.setTeamNumber((int) communicate.charAt(1));
+            return communicate.charAt(2);
+        }
+        else return 'x';
+    }
+
+    public boolean ifWaiting(String communicate) {
+        if(communicate.charAt(0) == 'w') {
+            //wait
+            return true;
+        }
+        else {
+            if (communicate.charAt(0) == 'm') return false;
+        }
+        return true;
+    }
+
+    public boolean ifMyMove(String communicate) {
+        if (communicate.charAt(0) == 'd') return true;
+        else return false;
+    }
+
+    public boolean ifEnd (String communicate) {
+        if (communicate.charAt(0) == 'x') return true;
+        else return false;
+    }
+
+    public int whoWin(String communicate) {
+        if (communicate.charAt(0) == 'u') {
+            if ((int)communicate.charAt(4) == this.getTeamNumber()) {
+                //yes you win
+                return this.getTeamNumber();
+            }
+            else return (int)communicate.charAt(4);
+        }
+        else return -1;
     }
 
     @Override
@@ -34,13 +83,23 @@ public class Client implements Runnable {
 
             DataOutputStream outToServer = new DataOutputStream(this.socket.getOutputStream());
             BufferedReader inFromServer = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
-            outToServer.writeBytes("1234" + "\n");
+
 
             while ((fromsrv = inFromServer.readLine()) != null) {
                 System.out.print(fromsrv);
+                checkCommunicate(fromsrv);
+                System.out.println(ifImNew(fromsrv));
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public Integer getTeamNumber() {
+        return teamNumber;
+    }
+
+    public void setTeamNumber(Integer teamNumber) {
+        this.teamNumber = teamNumber;
     }
 }
